a3699c268d6e22053f000b613098bf32e29cb436
vibhulala <bitwisenilay2006@gmail.com>
2026-08-11 21:42:32 +0530

    Time: 0 ms (100%), Space: 20.4 MB (14.09%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -1,8 +1,30 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
+        '''
+        Toh brute force mein hum sorted property completely ignore kar rahe hain
+        is liye ye brute force sirf normal understandind ke liy ehain 
+        hence  hm question me used propety ki help leke apna code design karenge
         count=0
         for i in range(len(grid)):
             for  j in range(len(grid[0])):
                 if grid[i][j]<0:
                     count+=1
         return count 
+        '''
+        m = len(grid)
+        n = len(grid[0])
+
+        row = m - 1
+        col = 0
+        count = 0
+
+        while row >= 0 and col < n:
+
+            if grid[row][col] < 0:
+                count += n - col
+                row -= 1
+
+            else:
+                col += 1
+
+        return count
