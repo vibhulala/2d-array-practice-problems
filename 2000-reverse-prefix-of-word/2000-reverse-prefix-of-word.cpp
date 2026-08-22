b47398513a53a1e9255772e5a8d46eeace3dc99a
vibhulala <bitwisenilay2006@gmail.com>
2026-08-22 22:57:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (99.53%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int j=word.find(ch);
+        int i=0;
+
+        while(i<j){
+            swap(word[i],word[j]);
+            i++;
+            j--;
+        }
+        return word ;
+    }
+};
