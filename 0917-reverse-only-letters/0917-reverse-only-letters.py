53b6450e15f6e4648461b1ec458330a6d87ee9df
vibhulala <bitwisenilay2006@gmail.com>
2026-09-04 00:57:21 +0530

    Time: 0 ms (100%), Space: 19.4 MB (37.71%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.py
+++ 0917-reverse-only-letters/0917-reverse-only-letters.py
@@ -1,5 +1,7 @@
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
+        '''brute force appraoch 
+        :- is approach mein hm diye gaye mixed string me se sare characters ko pic kar rhe hain after that picking process hmne  sare characters ko revrese kar diya aur letters nam ke ek list mein dal diya and then hmne ek result empt stirng banayi ab s ko fir se traverse kkuiiya agar alphbet hua to to letters mein ke index mein usko dal denge and result mein add kar deneg agra alphabet nhi ahua to as it is use result mein add kar denege 
         letters=[]
         #extracting all the letters 
         for ch in s:
@@ -17,3 +19,22 @@ class Solution:
             else:
                 result+=ch
         return result 
+        tc-o(n)
+        sc-o(n)
+        '''
+        chars=list(s)
+        left=0
+        right=len(chars)-1
+        while left<right:
+            #find a letter from left 
+            if not chars[left].isalpha():
+                left+=1
+            #find a letter from right 
+            elif not chars[right].isalpha():
+                right-=1
+            else:
+                #both are letters than sidha swap 
+                chars[left],chars[right]=chars[right],chars[left]
+                left+=1
+                right-=1
+        return "".join(chars)
